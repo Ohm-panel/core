7c4e420a8a266c86f5c8017d297a3466349f41f3
Joel Cogen <joel.cogen@student.umons.ac.be>
2009-11-18 20:29:51 +0100

    Functional test for Login controller

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionController::TestCase
+  fixtures :users
+
   test "index" do
-    assert true
+    get :index
+    assert_response :success
+  end
+
+  test "good login" do
+    user = users(:one)
+    user.password = "test1"
+    post :login, :user => user
+    assert_redirected_to users_path
+  end
+
+  test "bad login" do
+    user = users(:one)
+    user.password = "bad password"
+    post :login, :user => user
+    assert_redirected_to :action => :index
   end
 end
 
