5470fff45c836e2c86973c1a162aeeccbba087e2
Joel Cogen <joel.cogen@student.umons.ac.be>
2010-03-20 17:15:06 +0100

    Rails supprimé de vendor/rails (hum hum). Installer mis à jour

--- install.ubuntu-9.10.sh
+++ install.ubuntu-9.10.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# Ohm - Open Hosting Manager
+# Installer for Ubuntu Server 9.10
+#
+
+# Must run as root
+if ((`id -u` != 0)); then
+    echo "Must run as root"
+    exit 1
+fi
 
 # Brightbox repo for Phusion Passenger (mod_rails)
 echo "deb http://apt.brightbox.net hardy main" > /etc/apt/sources.list.d/brightbox.list
@@ -7,7 +17,7 @@ wget -q -O - http://apt.brightbox.net/release.asc | apt-key add -
 # Update and install APT packages
 apt-get update
 apt-get install \
-    ruby rubygems rails \
+    ruby ruby-dev rubygems rails \
     apache2 php5 libapache2-mod-passenger \
     mysql-server php5-mysql libmysql-ruby
 
