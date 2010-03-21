69013db83d20212c7ee9b5839830bc884a77c316
Joel Cogen <joel.cogen@student.umons.ac.be>
2010-03-22 00:34:45 +0100

    Installeur générique

--- installer/install/install.rb
+++ installer/install/install.rb
@@ -2,6 +2,8 @@
 # Installer
 
 require 'yaml'
+LOG = "ohm-install.log"
+STEPS = 4
 
 # Load distribution configuration
 @args = ARGV
@@ -31,11 +33,6 @@ class Dialog
     `#{dialog "inputbox", text, "--stdout"}`
   end
 
-  def tailbox(file)
-    system "echo \"\" >> /tmp/ohmtail"
-    system "dialog --title \"Installing required gems\" --no-kill --tailbox #{file} 0 0"
-  end
-
   def exit
     system "dialog --clear
             clear"
@@ -43,61 +40,51 @@ class Dialog
 end
 
 
+def exec(cmd)
+  system("(#{cmd}) >> #{LOG}")
+end
+
 # Welcome
 dialog = Dialog.new
-go = dialog.yesno("Welcome to the Ohm installer for #{cfg["distro"]}.\n\nProceed?")
+go = dialog.yesno("Welcome to the Ohm installer for #{cfg["distro"]}.\n
+Please verify this is your distribution and you are connected to the internet.
+Proceed?")
 exit 1 unless go
 
-# Brightbox repo for Phusion Passenger (mod_rails)
-dialog.progress(0, "Adding repository for Passenger Phusion (mod_rail)")
-File.open("/etc/apt/sources.list.d/brightbox.list", "w") { |f|
-  f.print File.read("/etc/apt/sources.list.d/brightbox.list")
-  f.print "deb http://apt.brightbox.net hardy main\n"
-}
-dialog.progress(50)
-system("wget -q -O - http://apt.brightbox.net/release.asc | apt-key add -")
-dialog.progress(100)
+# Phusion Passenger (mod_rails)
+dialog.progress(0, "Preparing Phusion Passenger (mod_rail)")
+exec(cfg["mod_rails"])
 
-
-# Update and install APT packages
-dialog.message("Will now install required APT packages")
-system "apt-get update
-        apt-get install \
-          ruby ruby-dev rubygems rails quota \
-          apache2 php5 libapache2-mod-passenger \
-          mysql-server php5-mysql libmysql-ruby"
+# Update and install packages
+dialog.progress((1/STEPS).to_i, "Installing required packages")
+exec(cfg["packages"])
 
 
 # Install requires Gems
-dialog.message("Will now install required Gems")
-#system "gem install rails -v 2.3.4 --no-rdoc --no-ri"
-#system "gem install fastthread --no-rdoc --no-ri"
+dialog.progress((2/STEPS).to_i, "Installing required gems")
+exec(cfg["gems"])
 
 
 # Configure Apache
-dialog.progress(0, "Configure Apache")
-File.open("#{cfg["apache_etc"]}/sites-available/ohm", "w") { |f|
-  f.print "<VirtualHost *:80>\n"
-  f.print "  RailsEnv development\n"                                            ### LIGNE A ENLEVER APRES TESTS !!!
-  f.print "  DocumentRoot $PANEL_PATH/public\n"
-  f.print "  <Directory $PANEL_PATH/public>\n"
-  f.print "    Allow from all\n"
-  f.print "    Options FollowSymLinks -MultiViews\n"
-  f.print "  </Directory>\n"
-  f.print "</VirtualHost>\n"
-}
-File.open("#{cfg["apache_etc"]}/apache2.conf", "w") { |f|
-  f.print File.read("#{cfg["apache_etc"]}/apache2.conf")
+dialog.progress((3/STEPS).to_i, "Configuring Apache")
+vhost = "<VirtualHost *:80>
+  RailsEnv development                                                          ### LIGNE A ENLEVER APRES TESTS !!!
+  DocumentRoot $PANEL_PATH/public
+  <Directory $PANEL_PATH/public>
+    Allow from all
+    Options FollowSymLinks -MultiViews
+  </Directory>
+</VirtualHost>"
+File.open("#{cfg["apache_sites"]}/ohm", "w") { |f| f.print vhost }
+File.open(cfg["apache_conf"], "w") { |f|
+  f.print File.read(cfg["apache_conf"])
   f.print "ServerName 0.0.0.0\n"
   f.print "NameVirtualHost *:80\n"
 }
-dialog.progress(25)
-system "a2ensite ohm"
-dialog.progress(50)
-system "a2dissite default"
-dialog.progress(75)
-system "service apache2 restart"
-dialog.progress(100)
+exec("a2ensite ohm")
+exec("a2dissite default")
+exec(cfg["apache_restart"])
 
+dialog.progress((4/STEPS).to_i, "Configuring Apache")
 dialog.exit
 
