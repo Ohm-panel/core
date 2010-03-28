3580309adecd08a3476d6b617cb7f132cb4abdd9
Joel Cogen <joel.cogen@student.umons.ac.be>
2010-03-28 18:52:48 +0200

    Module e-mail sorti du reste, compressé et pret pour install

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -32,26 +32,6 @@ ActiveRecord::Schema.define(:version => 20100328145425) do
     t.integer  "user_id"
   end
 
-  create_table "service_email_mailboxes", :force => true do |t|
-    t.string   "address"
-    t.integer  "domain_id"
-    t.string   "full_name"
-    t.integer  "size"
-    t.string   "password"
-    t.text     "forward"
-    t.boolean  "forward_only"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "service_email_users", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "max_mailboxes"
-    t.integer  "max_aliases"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "services", :force => true do |t|
     t.string   "name"
     t.text     "description"
