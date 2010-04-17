286cc3beaa1a1370c2afc682ae1c43428d484c77
Joel Cogen <joel@cloe>
2010-04-17 14:05:34 +0200

    Correction module e-mail

--- module_email/webapp/db/migrate/20100416150603_email_initialize_logs.rb
+++ module_email/webapp/db/migrate/20100416150603_email_initialize_logs.rb
@@ -1,4 +1,4 @@
-class InitializeEmailLogs < ActiveRecord::Migration
+class EmailInitializeLogs < ActiveRecord::Migration
   def self.up
     LogFile.create :name => "E-mail error", :path => "/var/log/mail.err"
     LogFile.create :name => "E-mail warning", :path => "/var/log/mail.warn"
